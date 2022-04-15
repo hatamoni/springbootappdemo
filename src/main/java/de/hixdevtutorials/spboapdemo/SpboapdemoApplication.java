a9db859d2956f89b858ca3056454fba559a312a4
Ibrahim <hatim.ibrahim@sanvartis.loc>
2022-04-15 12:40:33 +0200

    second commit

---
+++ src/main/java/de/hixdevtutorials/spboapdemo/SpboapdemoApplication.java
@@ -0,0 +1,13 @@
+package de.hixdevtutorials.spboapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpboapdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpboapdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hixdevtutorials/spboapdemo/SpboapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hixdevtutorials.spboapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpboapdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
